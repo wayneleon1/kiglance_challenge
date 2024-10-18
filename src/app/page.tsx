afb75006611bb5a133644ca0d7aea6eb8ea6ed17
wayneleon1 <rurangwaleon10@gmail.com>
2024-10-18 14:10:51 +0200

    configure redux toolkit and Material UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <div>Welcome Home </div>;
-}
+import React from "react";
+import Counter from "../components/Counter";
+
+const Home = () => {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-bold mb-8">Welcome to Kiglance</h1>
+      <Counter />
+    </div>
+  );
+};
+
+export default Home;
